b6f64dc71f1dc19a7e3b853a1553318612b80552
CFR2000 <https://github.com/CFR2000>
2022-12-10 10:36:59 +0000

    defined backpropagate step

--- src/main/MulitLayeredPerceptron.py
+++ src/main/MulitLayeredPerceptron.py
@@ -22,3 +22,19 @@ class MultilayeredPerceptron:
         output = sigmoid(np.dot(hidden, self.weights2) + self.bias2)
         return output
         
+    def backward(self, inputs, labels):
+        # Perform the backpropagation step
+        # Comput the error at the output layer
+        hidden = sigmoid(np.dot(inputs, self.weights1) + self.bias1)
+        output = sigmoid(np.dot(hidden, self.weights2) + self.bias2)
+        # Cost
+        error = labels - output
+
+        # Comput the error at the hidden layer
+        hidden_error = np.dot(error, self.weights2.T)
+
+        # Update the weights and biases of each layer
+        self.weights2 += self.learning_rate * np.dot(hidden.T, error)
+        self.bias2 += self.learning_rate * np.sum(error, axis=0)
+        self.weights1 += self.learning_rate * np.dot(inputs.T, hidden_error)
+        self.bias1 += self.learning_rate * np.sum(hidden_error, axis=0)
