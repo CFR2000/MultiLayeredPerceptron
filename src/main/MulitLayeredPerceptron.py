34c6530f7d722150aa1fb3dfffb3d6f7bcfa26a8
CFR2000 <https://github.com/CFR2000>
2022-12-10 10:30:04 +0000

    mlp class and defined init

--- src/main/MulitLayeredPerceptron.py
+++ src/main/MulitLayeredPerceptron.py
@@ -4,3 +4,14 @@ def sigmoid(x):
     # Define the sigmoid activation function
     return 1 / (1+np.exp(-x))
 
+class MultilayeredPerceptron:
+    def __init__(self, input_size, hidden_size, output_size, learning_rate):
+        # Initialize the weights and biases for each layer of the network
+        self.weights1 = np.random.rand(input_size, hidden_size) * 0.1
+        self.bias1 = np.random.rand(hidden_size) * 0.1
+        self.weights2 = np.random.rand(hidden_size, output_size) * 0.1
+        self.bias2 = np.random.rand(output_size) * 0.1
+
+        # Store the learning rate for the network
+        self.learning_rate = learning_rate
+        pass
