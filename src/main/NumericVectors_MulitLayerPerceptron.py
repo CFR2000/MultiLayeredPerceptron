5c24f6a71230865a6c7cd2ac0df2579331caaefd
CFR2000 <https://github.com/CFR2000>
2022-12-11 17:45:18 +0000

    Compartmentalized code sections.

--- src/main/NumericVectors_MulitLayerPerceptron.py
+++ src/main/NumericVectors_MulitLayerPerceptron.py
@@ -1,3 +1,9 @@
+# ==================================================================== #
+#                         Libraries                                    #
+# ==================================================================== #
+
+
+
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
@@ -6,6 +12,12 @@ import random
 import pandas as pd
 import matplotlib.pyplot as plt
 import math
+
+
+# ==================================================================== #
+#                    Activation Funcitons                              #
+# ==================================================================== #
+
 
 def sigmoid(x):
     # Define the sigmoid activation function
@@ -16,6 +28,13 @@ def tanh(x):
 
 def relu(x):
     return np.maximum(0, x)
+
+
+
+# ==================================================================== #
+#                   Start Of Neural Network                            #
+# ==================================================================== #
+
 
 class MultilayeredPerceptron:
     def __init__(self, input_size, hidden_size, output_size, learning_rate, activation):
@@ -79,9 +98,12 @@ class MultilayeredPerceptron:
         _, output_val = self.forward(inputs)
         return output_val
 
+    
+
 # ==================================================================== #
-#                   End of class
+#               Methods for running Training and Testing               #
 # ==================================================================== #
+
 
 #function to run training of model
 def run_training(network, x, y, epochs):
@@ -95,76 +117,103 @@ def run_training(network, x, y, epochs):
 
 
 #function to run testing of model
-def task2():
-
-    def make_data():
-        pass
-
-    def run_training(network, x, y, epochs):
-        for epoch in range(epochs):
-            for c, row in enumerate(x):
-                inputs = (np.asfarray(row[:]))
-                targets = (np.asfarray(y[c],dtype=float))
-                network.train(inputs, targets)
-            print(f"Epoch {epoch}: Training Occurring ...")
-        pass
-
-    # #load the data set
-    X = np.random.rand(500,4)
-    y = np.zeros(500)
-
-    for d in range(500): y[d] = math.sin((abs(X[d,0]-X[d,1]+X[d,2]-X[d,3])))
-
-    #Separate in Train and Test Datasets
-    X_train = X[0:400]
-    y_train = y[0:400]
-
-    X_test = X[400:501]
-    y_test = y[400:501]
-
-    #number of input, hidden and output nodes
-    input_nodes = 4
-    hidden_nodes = 5
-    output_nodes = 1
-
-    #learning rate
-    learning_rate = 0.1
-    activation = sigmoid
-
-
-    #create the instance
-    network = MultilayeredPerceptron(input_nodes, hidden_nodes, output_nodes, learning_rate, activation)
-
-    epochs = 1000
-
-    run_training(network, X_train, y_train, epochs)
-    # for epoch in range(epochs):
-    #     for counter, row in enumerate(X_train):
-    #         inputs = (np.asfarray(row[:]))
-    #         targets = (np.asfarray(y_train[counter],dtype=float))
-    #         network.train(inputs, targets)
-    #     print(f"Epoch {epoch}: Training Occurring ...")
-    
-
+def run_testing(network, x, y):
     num_correct = []
-    for count, row in enumerate(X_test):
+    for count, row in enumerate(x):
 
         inputs = (np.asfarray(row[:]))
         y_pred = network.predict(inputs)
 
-        true_l = y_test[count]
+        true_l = y[count]
         pred_l = y_pred[0,0]
         
         if (round(true_l,1)== round(pred_l,1)): num_correct.append(1)
         else: num_correct.append(0)
 
-    
-    #calculate the performance score, the fraction of correct answers
-    scorecard_array = np.asarray(num_correct)
-    print("Testing Performance Accuracy = " , scorecard_array.sum() / scorecard_array.size)
+    correct_array = np.asarray(num_correct)    
+    print("Testing Performance Accuracy = " , correct_array.sum() / correct_array.size)
+    pass
 
+
+# ==================================================================== #
+#                   Generate Data for Model                            #
+# ==================================================================== #
+
+
+def generate_data(rows, cols, train_size):
+    # #load the data set
+    X = np.random.rand(rows,cols)
+    y = np.zeros(rows)
+
+    for d in range(rows): y[d] = math.sin((abs(X[d,0]-X[d,1]+X[d,2]-X[d,3])))
+
+    train_size = int(rows*train_size)
+    #Separate in Train and Test Datasets
+    X_train = X[0:train_size] # 500*.8
+    y_train = y[0:train_size] # 500*.8
+
+    X_test = X[train_size:(rows+1)] # 500 + 1
+    y_test = y[train_size:(rows+1)] # 500 + 1
+    return X_train, y_train, X_test, y_test
+pass
+
+
+# ==================================================================== #
+#                       TESTING SUITIES                                #
+# ==================================================================== #
+
+
+def task2_100():
+
+    # load the data set.
+    X_train, y_train, X_test, y_test = generate_data(rows=500, cols=4, train_size=0.8)
+    # Number of epochs the network will be trained on.
+    epochs = 100
+    # Create a network.
+    network = MultilayeredPerceptron(input_size=4, hidden_size=5, output_size=1, learning_rate=0.1, activation=sigmoid)
+    # Train network.
+    run_training(network, X_train, y_train, epochs)
+    # Test Network Performance.
+    run_testing(network, X_test, y_test)
+
+
+def task2_1000():
+
+    # load the data set.
+    X_train, y_train, X_test, y_test = generate_data(rows=500, cols=4, train_size=0.8)
+    # Number of epochs the network will be trained on.
+    epochs = 1000
+    # Create a network.
+    network = MultilayeredPerceptron(input_size=4, hidden_size=50, output_size=1, learning_rate=0.1, activation=sigmoid)
+    # Train network.
+    run_training(network, X_train, y_train, epochs)
+    # Test Network Performance.
+    run_testing(network, X_test, y_test)
+
+
+def task2_10000():
+
+    # load the data set.
+    X_train, y_train, X_test, y_test = generate_data(rows=500, cols=4, train_size=0.8)
+    # Number of epochs the network will be trained on.
+    epochs = 10000
+    # Create a network.
+    network = MultilayeredPerceptron(input_size=4, hidden_size=500, output_size=1, learning_rate=0.1, activation=sigmoid)
+    # Train network.
+    run_training(network, X_train, y_train, epochs)
+    # Test Network Performance.
+    run_testing(network, X_test, y_test)
+
+
+
+# ==================================================================== #
+#                             MAIN                                     #s
+# ==================================================================== #
 
 
 if __name__ == "__main__":
-    task2()
+    task2_100()   # network = MultilayeredPerceptron(input_size=4, hidden_size=5, output_size=1, learning_rate=0.1, activation=sigmoid)
+    task2_1000()  # network = MultilayeredPerceptron(input_size=4, hidden_size=50, output_size=1, learning_rate=0.1, activation=sigmoid)
+    task2_10000() # network = MultilayeredPerceptron(input_size=4, hidden_size=500, output_size=1, learning_rate=0.1, activation=sigmoid)
+    
 
