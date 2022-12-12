cc496fb0b87374a6415d97a809cc094eddd8212d
CFR2000 <https://github.com/CFR2000>
2022-12-12 01:57:40 +0000

    added suite to plot errors

--- src/main/NumericVectors_MulitLayerPerceptron.py
+++ src/main/NumericVectors_MulitLayerPerceptron.py
@@ -11,7 +11,7 @@ import random
 import pandas as pd
 import matplotlib.pyplot as plt
 import math
-from visualise import confusion_visual
+from visualise import confusion_visual, plot_errors
 from sklearn.model_selection import GridSearchCV
 
 
@@ -122,6 +122,7 @@ def run_training(network, x, y, epochs, display_output=True):
     pass
 
 
+
 # function to run testing of model
 
 def run_testing(network, x, y):
@@ -168,7 +169,21 @@ def get_pred_values(network, x, y):
         predicted_labels.append(round(pred_l, 1))
     return np.array(true_labels), np.array(predicted_labels)
 
+def get_pred_values2(network, x, y):
+    true_labels = []
+    predicted_labels = []
+    for count, row in enumerate(x):
+        inputs = (np.asfarray(row[:]))
+        y_pred = network.predict(inputs)
 
+        true_l = y[count]
+        pred_l = y_pred[0, 0]
+        true_labels.append(true_l)
+        predicted_labels.append(pred_l)
+    return np.array(true_labels), np.array(predicted_labels)
+
+
+# Digitising the predictions and truths and gets the scores for the model.
 def evaluate(true, pred):
     # Digitize T,P values
     bins = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
@@ -233,6 +248,8 @@ def task2_100():
     true_labels, predicted_labels = get_pred_values(network, X_test, y_test)
     # visualise predictions
     confusion_visual(true_labels, predicted_labels, epochs)
+    # Visualise Errors 
+    plot_errors(true_labels, predicted_labels, epochs)
 
 
 def task2_1000():
@@ -241,7 +258,7 @@ def task2_1000():
     X_train, y_train, X_test, y_test = generate_data(
         rows=500, cols=4, train_size=0.8)
     # Number of epochs the network will be trained on.
-    epochs = 5000
+    epochs = 100
     # Create a network.
     network = MultilayeredPerceptron(
         input_size=4, hidden_size=40, output_size=1, learning_rate=0.1, activation=sigmoid)
@@ -250,9 +267,11 @@ def task2_1000():
     # Test Network Performance.
     run_testing(network, X_test, y_test)
     # get true and predicitons
-    true_labels, predicted_labels = get_pred_values(network, X_test, y_test)
+    true_labels, predicted_labels = get_pred_values2(network, X_test, y_test)
     # visualise predictions
     confusion_visual(true_labels, predicted_labels, epochs)
+    # Visualise Errors 
+    plot_errors(true_labels, predicted_labels, epochs)
 
 
 def task2_10000():
@@ -270,9 +289,11 @@ def task2_10000():
     # Test Network Performance.
     run_testing(network, X_test, y_test)
     # get true and predicitons
-    true_labels, predicted_labels = get_pred_values(network, X_test, y_test)
+    true_labels, predicted_labels = get_pred_values2(network, X_test, y_test)
     # visualise predictions
     confusion_visual(true_labels, predicted_labels, epochs)
+    # Visualise Errors 
+    plot_errors(true_labels, predicted_labels, epochs)
 
 def task2_25000():
 
@@ -292,6 +313,8 @@ def task2_25000():
     true_labels, predicted_labels = get_pred_values(network, X_test, y_test)
     # visualise predictions
     confusion_visual(true_labels, predicted_labels, epochs)
+    # Visualise Errors 
+    plot_errors(true_labels, predicted_labels, epochs)
 
 def task2_40000():
 
@@ -311,6 +334,8 @@ def task2_40000():
     true_labels, predicted_labels = get_pred_values(network, X_test, y_test)
     # visualise predictions
     confusion_visual(true_labels, predicted_labels, epochs)
+    # Visualise Errors 
+    plot_errors(true_labels, predicted_labels, epochs)
 
 def test():
     pass
@@ -322,8 +347,8 @@ def test():
 
 
 if __name__ == "__main__":
-    # task2_100()   # network = MultilayeredPerceptron(input_size=4, hidden_size=5, output_size=1, learning_rate=0.1, activation=sigmoid)
-    # task2_1000()  # network = MultilayeredPerceptron(input_size=4, hidden_size=50, output_size=1, learning_rate=0.1, activation=sigmoid)
+    task2_100()   # network = MultilayeredPerceptron(input_size=4, hidden_size=5, output_size=1, learning_rate=0.1, activation=sigmoid)
+    task2_1000()  # network = MultilayeredPerceptron(input_size=4, hidden_size=50, output_size=1, learning_rate=0.1, activation=sigmoid)
     # task2_10000() # network = MultilayeredPerceptron(input_size=4, hidden_size=500, output_size=1, learning_rate=0.1, activation=sigmoid)
     # task2_25000()
     # task2_40000()
